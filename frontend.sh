45a055b4f64b394a6fccd682b22ae7e855985c87
rajibr <82666308+rajibrajkhowa@users.noreply.github.com>
2025-03-19 11:48:31 +0530

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,26 +1,10 @@
 #! /bin/bash
 
-exit_tool()
-
-{
-
-if [ -f "final_output.txt" ];
-
-then
-   rm final_output.txt
-   exit
-else
-   exit
-fi
-
-}
-
-
 vip_status()
 
 {
 
-./script/F5_VIP_Status_Interactive.bash
+./script/F5_VIP_Status_Interactive.sh
 
 }
 
@@ -28,7 +12,7 @@ vs_status()
 
 {
 
-./script/F5_VS_Status_Interactive.bash
+./script/F5_VS_Status_Interactive.sh
 
 }
 
@@ -36,7 +20,7 @@ pool_status()
 
 {
 
-./script/F5_Pool_Status_Interactive.bash
+./script/F5_Pool_Status_Interactive.sh
 
 }
 
@@ -45,7 +29,15 @@ pool_member_status()
 
 {
 
-./script/F5_Pool_Member_Status_Interactive.bash
+./script/F5_Pool_Member_Status_Interactive.sh
+
+}
+
+vs_cert_binding()
+
+{
+
+./script/F5_VS_Cert_Bindings_Interactive.sh
 
 }
 
@@ -67,27 +59,28 @@ echo
 echo "
 Which tool you want to use today for analysis ?
 
-Choose 0 to exit the tool
 Choose 1 to check status of a given VIP or Virtual Address
 Choose 2 to check the status of a given Virtual Server
 Choose 3 to check the status of a given Node Pool
 Choose 4 to check the status of Pool Members of a given Node Pool
+Choose 5 to check Virtual Server and Certificate bindings
 "
 echo
 
 read -p "Please enter the option? " OPTION
 
-
 echo
 
 case "$OPTION" in
-(0)   exit_tool;;
 (1)   vip_status;;
 (2)   vs_status;;
 (3)   pool_status;;
-(4)   pool_member_status;;
+(3)   pool_member_statusstatus;;
+(5)   vs_cert_binding;;
 esac
 echo
 echo "#############################################"
 
 done
+
+exit
