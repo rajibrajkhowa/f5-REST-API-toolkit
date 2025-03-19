326d6bb1caeffa3c7595d91b5b7527717fd2e407
rajibr <82666308+rajibrajkhowa@users.noreply.github.com>
2025-03-19 15:46:05 +0530

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -84,11 +84,12 @@ echo "
 Which tool you want to use today for analysis ?
 
 Choose 0 to exit the tool
-Choose 1 to check status of a given VIP or Virtual Address
-Choose 2 to check the status of a given Virtual Server
-Choose 3 to check the status of a given Node Pool
-Choose 4 to check the status of Pool Members of a given Node Pool
-Choose 5 to check Virtual Server and Certificate bindings
+Choose 1 to get VIP details (Virtual Server & Node Pool) mapped to a VIP
+Choose 2 to check status of a given VIP or Virtual Address
+Choose 3 to check the status of a given Virtual Server
+Choose 4 to check the status of a given Node Pool
+Choose 5 to check the status of Pool Members of a given Node Pool
+Choose 6 to check Virtual Server and Certificate bindings
 "
 echo
 
